f7d3317c178433d883542daaef4b99f9ceb5eb7a
jumasy <jumasy1980@gmail.com>
2017-08-01 17:31:48 -0700

    added get_var(axis) options

--- helita/sim/bifrost.py
+++ helita/sim/bifrost.py
@@ -82,6 +82,7 @@ class BifrostData(object):
                                   " .idl files found"))
         self.snap = snap
         self.snap_str = '_%03i' % snap
+        self.axisvars = ['x', 'y', 'z']
         self.__read_params()
         # Read mesh for all snaps because meshfiles could differ
         self.__read_mesh(self.meshfile)
@@ -91,6 +92,7 @@ class BifrostData(object):
         if (self.do_mhd):
             self.snapvars += ['bx', 'by', 'bz']
         self.hionvars = []
+
         if 'do_hion' in self.params:
             if self.params['do_hion'] > 0:
                 self.hionvars = ['hionne', 'hiontg', 'n1',
@@ -161,7 +163,7 @@ class BifrostData(object):
             meshfile = os.path.join(self.fdir, self.params['meshfile'].strip())
         if os.path.isfile(meshfile):
             f = open(meshfile, 'r')
-            for p in ['x', 'y', 'z']:
+            for p in self.axisvars:
                 dim = int(f.readline().strip('\n').strip())
                 assert dim == getattr(self, 'n' + p)
                 # quantity
@@ -243,12 +245,16 @@ class BifrostData(object):
             self.set_snap(snap)
         if var in self.variables:  # is variable already loaded?
             return self.variables[var]
+        elif var in self.axisvars:
+            if var == 'x': return self.x
+            if var == 'y': return self.y
+            if var == 'z': return self.z
         elif var in self.compvars:
             return self._get_composite_var(var, *args, **kwargs)
         else:
             raise ValueError(("get_var: could not read variable"
                       "%s. Must be one of %s" % (var, str(self.variables.keys()
-                                           + self.compvars + ['x', 'y', 'z']))))
+                                           + self.compvars + self.axisvars))))
 
     def _get_simple_var(self, var, order='F', mode='r'):
         """
