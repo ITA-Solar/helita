ae48eadc8b4c0c82ec9bae541b6ff27e59942331
jumasy <jumasy1980@gmail.com>
2018-03-03 05:38:17 -0800

    function that allows to check which line is the strongest one

--- helita/sim/atom_tools.py
+++ helita/sim/atom_tools.py
@@ -1531,9 +1531,6 @@ def fig_norm_ne_goftab_all():
         plt.xlabel("Pressure")
         fig.savefig('pressure_dependence_%s.png'% files[ifile][2:-4] ,bbox_inches="tight", format="png", dpi=650)
 
-
-
-
 def print_all_max():
 
     import pickle
@@ -1547,6 +1544,24 @@ def print_all_max():
         fileh=open(files[ifile],'rb')
         table=pickle.load(fileh)
         print(files[ifile],np.max(table.Gofnt['gofnt']))
+
+def print_comp_lines():
+
+    import pickle
+    import glob
+    import matplotlib.pyplot as plt
+
+    files=glob.glob("./*.opy")
+
+    nfiles=np.size(files)
+    for ifile in range(0,nfiles):
+        fileh=open(files[ifile],'rb')
+        table=pickle.load(fileh)
+        fileb=open(files[ifile],'rb')
+        high=pickle.load(fileb)
+        high.gofnt(wvlRange=[table.Gofnt['wvl']-0.1,table.Gofnt['wvl']+0.1],top=1,plot=False)
+        print(files[ifile],table.Gofnt['wvl'],high.Gofnt['wvl'])
+
 
 def fig_norm_ne_goftab_comb(namelist="./*_10?.*.opy"):
 
