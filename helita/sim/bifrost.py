d627e8b296921519d9614212befc704b982970b2
jumasy <jumasy1980@gmail.com>
2018-03-07 04:44:45 -0800

    removed fft from bifrost.py

--- helita/sim/bifrost.py
+++ helita/sim/bifrost.py
@@ -855,54 +855,6 @@ class BifrostData(object):
                               ' see e.g. self._get_quantity? for guidance'
                               '.' % (quant, repr(self.simple_vars))))
 
-
-    def fftTimeCube(self, quantity, snap, iix = None, iiy = None, iiz = None):
-
-        preTransform = self.get_varTime(quantity, snap, iix, iiy, iiz)
-        print(np.shape(preTransform))
-        arrShape = preTransform.shape
-        indexes = []
-
-        for num in arrShape:
-            if num == 1:
-                indexes.append(0)
-            else:
-                indexes.append(slice(None))
-
-        preTransform = preTransform[indexes[0], indexes[1], indexes[2], indexes[3]]
-        transformed = np.empty((preTransform.shape))
-        dt = self.params['dt']
-        t = self.params['t']
-
-        import time
-        t0 = time.time()
-        # @numba.jit(['float64[:, :, ::1](float64[:, :, ::1], float64[:, :, ::1], float64[::1], float64[::1])'])
-        def fftHelper(preTransform, transformed, dt, t):
-
-            uneven = False
-            for i in range(1, np.size(dt) - 1):
-                if abs((t[i] - t[i -1]) - (t[i+1] - t[i])) > 0.02:
-                    uneven = True
-                    break
-
-            if uneven:
-                print('uneven dt')
-                evenTimes = np.linspace(t[0], t[-1], np.size(dt))
-                interp = sp.interpolate.interp1d(t, preTransform)
-                preTransform = interp(evenTimes)
-                evenDt = evenTimes[1] - evenTimes[0]
-            else:
-                evenDt = dt[0]
-
-            freq = np.fft.fftshift(np.fft.fftfreq(np.size(dt), evenDt * 100))
-            transformed = np.abs(np.fft.fftshift((np.fft.fft(preTransform)), axes = -1))
-            output = {'freq': freq, 'ftCube': transformed}
-            t1 = time.time()
-            print(t1-t0)
-            return output
-
-        return fftHelper(preTransform, transformed, dt, t)
-
     def write_rh15d(self, outfile, desc=None, append=True,
                     sx=slice(None), sy=slice(None), sz=slice(None)):
         """
