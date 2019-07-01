d8fcec51d19002baba9ca84d12c76a56e5c6bee1
Juan <jumasy1980@gmail.com>
2019-07-01 08:02:06 -0700

    towards a generic bifrost_units class

--- helita/sim/bifrost.py
+++ helita/sim/bifrost.py
@@ -1923,120 +1923,142 @@ def cartesian2polar(x, y, grid, r, t, order=3):
                            order=order).reshape(new_x.shape)
 
 
-class Bifrost_units():
-    import scipy.constants as const
-    from astropy import constants as aconst
+class Bifrost_units(object):
+
     """
     Bifrost_units.py
     Created by Mikolaj Szydlarski on 2017-01-20.
     Copyright (c) 2014, ITA UiO - All rights reserved.
     """
 
-    u_l = 1e8
-    u_t = 1e2
-    u_r = 1e-7
-    u_u = u_l / u_t
-    u_p = u_r * (u_l / u_t)**2          # Pressure [dyne/cm2]
-    u_kr = 1 / (u_r * u_l)               # Rosseland opacity [cm2/g]
-    u_ee = u_u**2
-    u_e = u_r * u_ee
-    u_te = u_e / u_t * u_l               # Box therm. em. [erg/(s ster cm2)]
-    mu = 0.8
-    u_n = 3.00e+10                  # Denisty number n_0 * 1/cm^3
-    k_b = aconst.k_B.to('erg/K')  # 1.380658E-16 Boltzman's cst. [erg/K]
-    m_h = const.m_n / const.gram  # 1.674927471e-24
-    m_he = 6.65e-24
-    m_p = mu * m_h   # Mass per particle
-    m_e = const.m_e / const.gram  # 9.1093897E-28
-    u_tg = (m_h / k_b) * u_ee
-    u_tge = (m_e / k_b) * u_ee
-    pi = const.pi
-    u_b = u_u * np.sqrt(4. * pi * u_r)
+    def __init__(self,filename='mhd.in'):
+        import scipy.constants as const
+        from astropy import constants as aconst
+        if os.path.isfile(filename):
+            self.params = read_idl_ascii(filename)
+            try:
+                self.u_l = self.params['u_l']
+                self.u_t = self.params['u_t']
+                self.u_r = self.params['u_r']
+                # --- ideal gas
+                self.gamma = self.params['gamma']
 
-    usi_l = u_l * const.centi  # 1e6
-    usi_r = u_r * const.gram  # 1e-4
-    usi_u = usi_l / u_t
-    usi_p = usi_r * (usi_l / u_t)**2       # Pressure [N/m2]
-    usi_kr = 1 / (usi_r * usi_l)            # Rosseland opacity [m2/kg]
-    usi_ee = usi_u**2
-    usi_e = usi_r * usi_ee
-    usi_te = usi_e / u_t * usi_l            # Box therm. em. [J/(s ster m2)]
-    ksi_b = aconst.k_B.to('J/K')  # 1.380658E-23 Boltzman's cst. [J/K]
-    msi_h = const.m_n  # 1.674927471e-27
-    msi_he = 6.65e-27
-    msi_p = mu * msi_h  # Mass per particle
-    usi_tg = (msi_h / ksi_b) * usi_ee
-    msi_e = const.m_e  # 9.1093897e-31
-    usi_b = u_b * 1e-4
+            except:
+                print('(WWW) the filename does not have u_l, u_t and u_r.'
+                    ' Default Solar Bifrost units has been selected')
+                self.u_l = 1.0e8
+                self.u_t = 1.0e2
+                self.u_r = 1.0e-7
+                # --- ideal gas
+                self.gamma = self.params['gamma']
+        else:
+            print('(WWW) selected filename is not available.'
+                ' Default Solar Bifrost units has been selected')
+            self.u_l = 1.0e8
+            self.u_t = 1.0e2
+            self.u_r = 1.0e-7
+            # --- ideal gas
+            self.gamma = self.params['gamma']
 
-    # Solar gravity
-    gsun = 27400.0  # (cgs)
+        self.u_u = self.u_l / self.u_t
+        self.u_p = self.u_r * (self.u_l / self.u_t)**2          # Pressure [dyne/cm2]
+        self.u_kr = 1 / (self.u_r * self.u_l)               # Rosseland opacity [cm2/g]
+        self.u_ee = self.u_u**2
+        self.u_e = self.u_r * self.u_ee
+        self.u_te = self.u_e / self.u_t * self.u_l               # Box therm. em. [erg/(s ster cm2)]
+        self.mu = 0.8
+        self.u_n = 3.00e+10                  # Denisty number n_0 * 1/cm^3
+        self.k_b = aconst.k_B.to('erg/K')  # 1.380658E-16 Boltzman's cst. [erg/K]
+        self.m_h = const.m_n / const.gram  # 1.674927471e-24
+        self.m_he = 6.65e-24
+        self.m_p = self.mu * self.m_h   # Mass per particle
+        self.m_e = const.m_e / const.gram  # 9.1093897E-28
+        self.u_tg = (self.m_h / self.k_b) * self.u_ee
+        self.u_tge = (self.m_e / self.k_b) * self.u_ee
+        self.pi = const.pi
+        self.u_b = self.u_u * np.sqrt(4. * self.pi * self.u_r)
 
-    # --- ideal gas
-    gamma = 1.667
+        self.usi_l = self.u_l * const.centi  # 1e6
+        self.usi_r = self.u_r * const.gram  # 1e-4
+        self.usi_u = self.usi_l / self.u_t
+        self.usi_p = self.usi_r * (self.usi_l / self.u_t)**2       # Pressure [N/m2]
+        self.usi_kr = 1 / (self.usi_r * self.usi_l)            # Rosseland opacity [m2/kg]
+        self.usi_ee = self.usi_u**2
+        self.usi_e = self.usi_r * self.usi_ee
+        self.usi_te = self.usi_e / self.u_t * self.usi_l            # Box therm. em. [J/(s ster m2)]
+        self.ksi_b = aconst.k_B.to('J/K')  # 1.380658E-23 Boltzman's cst. [J/K]
+        self.msi_h = const.m_n  # 1.674927471e-27
+        self.msi_he = 6.65e-27
+        self.msi_p = self.mu * self.msi_h  # Mass per particle
+        self.usi_tg = (self.msi_h / self.ksi_b) * self.usi_ee
+        self.msi_e = const.m_e  # 9.1093897e-31
+        self.usi_b = self.u_b * 1e-4
 
-    # --- physical constants and other useful quantities
-    clight = aconst.c.to('cm/s')  # 2.99792458E+10 Speed of light [cm/s]
-    hplanck = aconst.h.to('erg s')  # 6.6260755E-27 Planck's constant [erg s]
-    kboltzmann = aconst.k_B.to('erg/K')  # 1.380658E-16 Boltzman's cst. [erg/K]
-    amu = aconst.u.to('g')  # 1.6605402E-24 Atomic mass unit [g]
-    amusi = aconst.u.to('kg')  # 1.6605402E-27 Atomic mass unit [kg]
-    m_electron = aconst.m_e.to('g')  # 9.1093897E-28 Electron mass [g]
-    q_electron = 4.80325E-10    # Electron charge [esu]
-    qsi_electron = aconst.e  # 1.6021765e-19 Electron charge [C]
-    rbohr = aconst.a0.to('cm')  # 5.29177349e-9 bohr radius [cm]
-    e_rydberg = 2.1798741e-11  # ion. pot. hydrogen [erg]
-    eh2diss = 4.478          # H2 dissociation energy [eV]
-    pie2_mec = 0.02654        # pi e^2 / m_e c [cm^2 Hz]
-    # 5.670400e-5 Stefan-Boltzmann constant [erg/(cm^2 s K^4)]
-    stefboltz = aconst.sigma_sb.to('erg/(cm2 s K4)')
-    mion = m_h            # Ion mass [g]
-    r_ei = 1.44E-7        # e^2 / kT = 1.44x10^-7 T^-1 cm
+        # Solar gravity
+        self.gsun = 27400.0  # (cgs)
 
-    # --- Unit conversions
-    ev_to_erg = const.eV / const.erg  # 1.60217733e-12 one electronvolt [erg]
-    ev_to_j = const.eV  # 1.60217733e-19 one electronvolt [j]
-    nm_to_m = const.nano  # 1.0e-09
-    cm_to_m = const.centi  # 1.0e-02
-    km_to_m = const.kilo  # 1.0e+03
-    erg_to_joule = const.erg  # 1.0e-07
-    g_to_kg = const.gram  # 1.0e-03
-    micron_to_nm = 1.0e+03
-    megabarn_to_m2 = 1.0e-22
-    atm_to_pa = const.atm  # 1.0135e+05 atm to pascal (n/m^2)
-    dyne_cm2_to_pascal = 0.1
-    k_to_ev = 8.621738E-5    # KtoeV
-    ev_to_k = 11604.50520    # eVtoK
-    ergd2wd = 0.1
-    grph = 2.27e-24
-    permsi = 8.85e-12  # Permitivitty in vacuum (F/m)
-    cross_p = 1.59880e-14
-    cross_he = 9.10010e-17
+        # --- physical constants and other useful quantities
+        self.clight = aconst.c.to('cm/s')  # 2.99792458E+10 Speed of light [cm/s]
+        self.hplanck = aconst.h.to('erg s')  # 6.6260755E-27 Planck's constant [erg s]
+        self.kboltzmann = aconst.k_B.to('erg/K')  # 1.380658E-16 Boltzman's cst. [erg/K]
+        self.amu = aconst.u.to('g')  # 1.6605402E-24 Atomic mass unit [g]
+        self.amusi = aconst.u.to('kg')  # 1.6605402E-27 Atomic mass unit [kg]
+        self.m_electron = aconst.m_e.to('g')  # 9.1093897E-28 Electron mass [g]
+        self.q_electron = 4.80325E-10    # Electron charge [esu]
+        self.qsi_electron = aconst.e  # 1.6021765e-19 Electron charge [C]
+        self.rbohr = aconst.a0.to('cm')  # 5.29177349e-9 bohr radius [cm]
+        self.e_rydberg = 2.1798741e-11  # ion. pot. hydrogen [erg]
+        self.eh2diss = 4.478          # H2 dissociation energy [eV]
+        self.pie2_mec = 0.02654        # pi e^2 / m_e c [cm^2 Hz]
+        # 5.670400e-5 Stefan-Boltzmann constant [erg/(cm^2 s K^4)]
+        self.stefboltz = aconst.sigma_sb.to('erg/(cm2 s K4)')
+        self.mion = self.m_h            # Ion mass [g]
+        self.r_ei = 1.44E-7        # e^2 / kT = 1.44x10^-7 T^-1 cm
 
-    # Dissociation energy of H2 [eV] from Barklem & Collet (2016)
-    di = 4.478007
+        # --- Unit conversions
+        self.ev_to_erg = const.eV / const.erg  # 1.60217733e-12 one electronvolt [erg]
+        self.ev_to_j = const.eV  # 1.60217733e-19 one electronvolt [j]
+        self.nm_to_m = const.nano  # 1.0e-09
+        self.cm_to_m = const.centi  # 1.0e-02
+        self.km_to_m = const.kilo  # 1.0e+03
+        self.erg_to_joule = const.erg  # 1.0e-07
+        self.g_to_kg = const.gram  # 1.0e-03
+        self.micron_to_nm = 1.0e+03
+        self.megabarn_to_m2 = 1.0e-22
+        self.atm_to_pa = const.atm  # 1.0135e+05 atm to pascal (n/m^2)
+        self.dyne_cm2_to_pascal = 0.1
+        self.k_to_ev = 8.621738E-5    # KtoeV
+        self.ev_to_k = 11604.50520    # eVtoK
+        self.ergd2wd = 0.1
+        self.grph = 2.27e-24
+        self.permsi = 8.85e-12  # Permitivitty in vacuum (F/m)
+        self.cross_p = 1.59880e-14
+        self.cross_he = 9.10010e-17
 
-    atomdic = {'h': 1, 'he': 2, 'c': 3, 'n': 4, 'o': 5, 'ne': 6, 'na': 7,
-               'mg': 8, 'al': 9, 'si': 10, 's': 11, 'k': 12, 'ca': 13,
-               'cr': 14, 'fe': 15, 'ni': 16}
-    abnddic = {'h': 12.0, 'he': 11.0, 'c': 8.55, 'n': 7.93, 'o': 8.77,
-               'ne': 8.51, 'na': 6.18, 'mg': 7.48, 'al': 6.4, 'si': 7.55,
-               's': 5.21, 'k': 5.05, 'ca': 6.33, 'cr': 5.47, 'fe': 7.5,
-               'ni': 5.08}
-    weightdic = {'h': 1.008, 'he': 4.003, 'c': 12.01, 'n': 14.01,
-                 'o': 16.00, 'ne': 20.18, 'na': 23.00, 'mg': 24.32,
-                 'al': 26.97, 'si': 28.06, 's': 32.06, 'k': 39.10,
-                 'ca': 40.08, 'cr': 52.01, 'fe': 55.85, 'ni': 58.69}
-    xidic = {'h': 13.595, 'he': 24.580, 'c': 11.256, 'n': 14.529,
-             'o': 13.614, 'ne': 21.559, 'na': 5.138, 'mg': 7.644,
-             'al': 5.984, 'si': 8.149, 's': 10.357, 'k': 4.339,
-             'ca': 6.111, 'cr': 6.763, 'fe': 7.896, 'ni': 7.633}
-    u0dic = {'h': 2., 'he': 1., 'c': 9.3, 'n': 4., 'o': 8.7,
-             'ne': 1., 'na': 2., 'mg': 1., 'al': 5.9, 'si': 9.5, 's': 8.1,
-             'k': 2.1, 'ca': 1.2, 'cr': 10.5, 'fe': 26.9, 'ni': 29.5}
-    u1dic = {'h': 1., 'he': 2., 'c': 6., 'n': 9.,  'o': 4.,  'ne': 5.,
-             'na': 1., 'mg': 2., 'al': 1., 'si': 5.7, 's': 4.1, 'k': 1.,
-             'ca': 2.2, 'cr': 7.2, 'fe': 42.7, 'ni': 10.5}
+        # Dissociation energy of H2 [eV] from Barklem & Collet (2016)
+        self.di = 4.478007
+
+        self.atomdic = {'h': 1, 'he': 2, 'c': 3, 'n': 4, 'o': 5, 'ne': 6, 'na': 7,
+                   'mg': 8, 'al': 9, 'si': 10, 's': 11, 'k': 12, 'ca': 13,
+                   'cr': 14, 'fe': 15, 'ni': 16}
+        self.abnddic = {'h': 12.0, 'he': 11.0, 'c': 8.55, 'n': 7.93, 'o': 8.77,
+                   'ne': 8.51, 'na': 6.18, 'mg': 7.48, 'al': 6.4, 'si': 7.55,
+                   's': 5.21, 'k': 5.05, 'ca': 6.33, 'cr': 5.47, 'fe': 7.5,
+                   'ni': 5.08}
+        self.weightdic = {'h': 1.008, 'he': 4.003, 'c': 12.01, 'n': 14.01,
+                     'o': 16.00, 'ne': 20.18, 'na': 23.00, 'mg': 24.32,
+                     'al': 26.97, 'si': 28.06, 's': 32.06, 'k': 39.10,
+                     'ca': 40.08, 'cr': 52.01, 'fe': 55.85, 'ni': 58.69}
+        self.xidic = {'h': 13.595, 'he': 24.580, 'c': 11.256, 'n': 14.529,
+                 'o': 13.614, 'ne': 21.559, 'na': 5.138, 'mg': 7.644,
+                 'al': 5.984, 'si': 8.149, 's': 10.357, 'k': 4.339,
+                 'ca': 6.111, 'cr': 6.763, 'fe': 7.896, 'ni': 7.633}
+        self.u0dic = {'h': 2., 'he': 1., 'c': 9.3, 'n': 4., 'o': 8.7,
+                 'ne': 1., 'na': 2., 'mg': 1., 'al': 5.9, 'si': 9.5, 's': 8.1,
+                 'k': 2.1, 'ca': 1.2, 'cr': 10.5, 'fe': 26.9, 'ni': 29.5}
+        self.u1dic = {'h': 1., 'he': 2., 'c': 6., 'n': 9.,  'o': 4.,  'ne': 5.,
+                 'na': 1., 'mg': 2., 'al': 1., 'si': 5.7, 's': 4.1, 'k': 1.,
+                 'ca': 2.2, 'cr': 7.2, 'fe': 42.7, 'ni': 10.5}
 
 
 class Rhoeetab:
