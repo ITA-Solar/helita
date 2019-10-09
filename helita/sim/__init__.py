18e3bbf5b0324db363784c3f7bcd349f827efe42
Juan <jumasy1980@gmail.com>
2019-10-08 17:22:46 -0700

    imports ebysus if at_tools is in there

--- helita/sim/__init__.py
+++ helita/sim/__init__.py
@@ -4,10 +4,24 @@ and radiative transfer codes. Also includes routines for working
 with synthetic spectra.
 """
 
-__all__ = ["bifrost", "multi", "multi3d", "muram", "rh", "rh15d", "simtools",
-           "synobs"]
+import imp
+
+try:
+    imp.find_module('at_tools')
+    found = True
+except ImportError:
+    found = False
+
+if found:
+    __all__ = ["bifrost", "multi", "multi3d", "muram", "rh", "rh15d",
+            "simtools", "synobs", "ebysus"]
+else:
+    __all__ = ["bifrost", "multi", "multi3d", "muram", "rh", "rh15d",
+            "simtools", "synobs"]
 
 from . import bifrost
 from . import multi
 from . import muram
 from . import rh
+if found:
+    from . import ebysus
