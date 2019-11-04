dca53ce98200101af1421176bec731a73b09eb5b
Juan <jumasy1980@gmail.com>
2019-11-04 11:03:52 -0800

    [FIX] typo when merging

--- helita/sim/__init__.py
+++ helita/sim/__init__.py
@@ -4,7 +4,6 @@ and radiative transfer codes. Also includes routines for working
 with synthetic spectra.
 """
 
-<<<<<<< HEAD
 import imp
 
 try:
@@ -19,17 +18,11 @@ if found:
 else:
     __all__ = ["bifrost", "multi", "multi3d", "muram", "rh", "rh15d",
             "simtools", "synobs"]
-=======
-__all__ = ["bifrost", "multi", "multi3d", "muram", "rh", "rh15d", "simtools",
-           "synobs"]
->>>>>>> 6de7438e13986907dc1e92fd39741f3632421b57
+
 
 from . import bifrost
 from . import multi
 from . import muram
 from . import rh
-<<<<<<< HEAD
 if found:
     from . import ebysus
-=======
->>>>>>> 6de7438e13986907dc1e92fd39741f3632421b57
