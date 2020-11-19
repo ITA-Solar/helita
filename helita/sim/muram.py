850f9d9dc9a86af3a0afd6ca0fe221d5e85fc68f
juan <jumasy1980@gmail.com>
2020-11-19 11:11:49 +0000

    [FIX] make sure that conversion exists

--- helita/sim/muram.py
+++ helita/sim/muram.py
@@ -219,7 +219,7 @@ class MuramAtmos:
             varu=var.replace('x','')
             varu=varu.replace('y','')
             varu=varu.replace('z','')
-            if var in self.varn.keys(): 
+            if (var in self.varn.keys()) and (varu in self.uni.keys()): 
                 cgsunits = self.uni[varu]
             else: 
                 cgsunits = 1.0
