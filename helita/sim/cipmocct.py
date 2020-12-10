[FIX] units is a class now

helita/sim/cipmocct.py
@@ -30,7 +30,8 @@ class Cipmocct:
     self.snap = snap 
     self.sel_units = sel_units
     self.verbose = verbose
-
+    self.uni = Cipmocct_units()
+    
     params = rsav(os.path.join(self.fdir,'params_'+rootname+'.sav'))
     
     self.x = params['x1']
@@ -57,7 +58,6 @@ class Cipmocct:
     self.cstagop = False # This will not allow to use cstagger from Bifrost in load
     self.hion = False # This will not allow to use HION from Bifrost in load
 
-    self.units()
     self.genvar()
 
 
@@ -105,8 +105,8 @@ class Cipmocct:
         varu=var.replace('x','')
         varu=varu.replace('y','')
         varu=varu.replace('z','')
-        if (var in self.varn.keys()) and (varu in self.uni.keys()): 
-          cgsunits = self.uni[varu]
+        if (var in self.varn.keys()) and (varu in self.uni.uni.keys()): 
+          cgsunits = self.uni.uni[varu]
         else: 
           cgsunits = 1.0
       else: 
@@ -124,11 +124,14 @@ class Cipmocct:
       self.data = load_noeos_quantities(self,var)
 
       if np.shape(self.data) == ():
-        self.data = load_quantities(self,var,PLASMA_QUANT='',
-                CYCL_RES='', COLFRE_QUANT='', COLFRI_QUANT='',
-                IONP_QUANT='', EOSTAB_QUANT='', TAU_QUANT='',
-                DEBYE_LN_QUANT='', CROSTAB_QUANT='',
-                COULOMB_COL_QUANT='', AMB_QUANT='')
+        self.data = load_quantities(self,var,PLASMA_QUANT='', CYCL_RES='',
+                COLFRE_QUANT='', COLFRI_QUANT='', IONP_QUANT='',
+                EOSTAB_QUANT='', TAU_QUANT='', DEBYE_LN_QUANT='',
+                CROSTAB_QUANT='', COULOMB_COL_QUANT='', AMB_QUANT='', 
+                HALL_QUANT='', BATTERY_QUANT='', SPITZER_QUANT='', 
+                KAPPA_QUANT='', GYROF_QUANT='', WAVE_QUANT='', 
+                FLUX_QUANT='', CURRENT_QUANT='', COLCOU_QUANT='',  
+                COLCOUMS_QUANT='', COLFREMX_QUANT='')
 
         # Loading arithmetic quantities
         if np.shape(self.data) == ():
@@ -148,30 +151,6 @@ class Cipmocct:
    
     return self.data
    
-
-  def units(self): 
-    '''
-    Units and constants in cgs
-    '''
-    self.uni={}
-
-    self.uni['gamma']  = 5./3.
-    self.uni['tg']     = 1.0e6 # K
-    self.uni['fact']   = 2
-    self.uni['l']      = 1000.*self.uni['fact']*1.0e5 # for having a 2000 km wide loop
-    self.uni['n']      = 1.0e9 # cm^-3    
-    self.uni['t']      = 1.0 # seconds
-
-    # Units and constants in SI
-    convertcsgsi(self)
-
-    globalvars(self)
-
-    self.uni['rho']    = self.uni['n'] * self.uni['proton'] /2. # gr cm^-3 
-    self.uni['u']      = np.sqrt(2*self.uni['gamma']*self.k_b/self.m_p*self.uni['tg']) # cm/s
-    self.uni['b']      = self.uni['u']*np.sqrt(self.uni['rho']) # Gauss
-    self.uni['j']      = self.uni['b']/self.uni['l']*self.uni['c'] # current density
-
  
   def genvar(self): 
     '''
@@ -224,5 +203,33 @@ class Cipmocct:
     # also velocities. 
 
     return var
+
+
+
+class Cipmocct_units(object): 
+
+    def __init__(self):
+
+        '''
+        Units and constants in cgs
+        '''
+        self.uni={}
+
+        self.uni['gamma']  = 5./3.
+        self.uni['tg']     = 1.0e6 # K
+        self.uni['fact']   = 2
+        self.uni['l']      = 1000.*self.uni['fact']*1.0e5 # for having a 2000 km wide loop
+        self.uni['n']      = 1.0e9 # cm^-3    
+        self.uni['t']      = 1.0 # seconds
+
+        # Units and constants in SI
+        convertcsgsi(self)
+
+        globalvars(self)
+
+        self.uni['rho']    = self.uni['n'] * self.uni['proton'] /2. # gr cm^-3 
+        self.uni['u']      = np.sqrt(2*self.uni['gamma']*self.k_b/self.m_p*self.uni['tg']) # cm/s
+        self.uni['b']      = self.uni['u']*np.sqrt(self.uni['rho']) # Gauss
+        self.uni['j']      = self.uni['b']/self.uni['l']*self.uni['c'] # current density
 
 

helita/sim/laresav.py
@@ -29,7 +29,8 @@ class Laresav:
     self.snap = snap 
     self.sel_units = sel_units
     self.verbose = verbose
-
+    self.uni = Laresav_units()
+    
     self.time     = self.savefile['d']['time'][0]
     self.time_prev= self.savefile['d']['time_prev'][0]
     self.timestep = self.savefile['d']['timestep'][0]
@@ -63,7 +64,6 @@ class Laresav:
     self.hion = False # This will not allow to use HION from Bifrost in load
 
     self.genvar()
-    self.units()
 
   def get_var(self,var,snap=None, iix=None, iiy=None, iiz=None, layout=None): 
     '''
@@ -116,8 +116,8 @@ class Laresav:
         varu=var.replace('x','')
         varu=varu.replace('y','')
         varu=varu.replace('z','')
-        if (var in self.varn.keys()) and (varu in self.uni.keys()): 
-          cgsunits = self.uni[varu]
+        if (var in self.varn.keys()) and (varu in self.uni.uni.keys()): 
+          cgsunits = self.uni.uni[varu]
         else: 
           cgsunits = 1.0
       else: 
@@ -143,11 +143,14 @@ class Laresav:
 
 
       if np.shape(self.data) == ():
-        self.data = load_quantities(self,var,PLASMA_QUANT='',
-                CYCL_RES='', COLFRE_QUANT='', COLFRI_QUANT='',
-                IONP_QUANT='', EOSTAB_QUANT='', TAU_QUANT='',
-                DEBYE_LN_QUANT='', CROSTAB_QUANT='',
-                COULOMB_COL_QUANT='', AMB_QUANT='')
+        self.data = load_quantities(self,var,PLASMA_QUANT='', CYCL_RES='',
+                COLFRE_QUANT='', COLFRI_QUANT='', IONP_QUANT='',
+                EOSTAB_QUANT='', TAU_QUANT='', DEBYE_LN_QUANT='',
+                CROSTAB_QUANT='', COULOMB_COL_QUANT='', AMB_QUANT='', 
+                HALL_QUANT='', BATTERY_QUANT='', SPITZER_QUANT='', 
+                KAPPA_QUANT='', GYROF_QUANT='', WAVE_QUANT='', 
+                FLUX_QUANT='', CURRENT_QUANT='', COLCOU_QUANT='',  
+                COLCOUMS_QUANT='', COLFREMX_QUANT='')
 
         # Loading arithmetic quantities
         if np.shape(self.data) == ():
@@ -167,27 +170,6 @@ class Laresav:
    
     return self.data
 
-  
-  def units(self): 
-    '''
-    Units and constants in cgs
-    '''
-    self.uni={}
-    self.uni['gamma']  = 5./3.
-    self.uni['tg']     = 5.77e9 # K
-    self.uni['l']      = 1.0e8 # Mm -> cm
-    self.uni['rho']    = 1.67e-9 # gr cm^-3 
-    self.uni['u']      = 6.9e8 # cm/s
-    self.uni['b']      = 100.0 # Gauss
-    self.uni['t']      = 0.145 # seconds
-    
-    # Units and constants in SI
-
-    convertcsgsi(self)
-
-    globalvars(self)
-
-    self.uni['n']      = self.uni['rho'] / self.m_p / 2. # cm^-3
 
   def genvar(self): 
     '''
@@ -247,3 +229,26 @@ class Laresav:
     return var
 
 
+class Laresav_units(object): 
+
+    def __init__(self):
+
+        '''
+        Units and constants in cgs
+        '''
+        self.uni={}
+        self.uni['gamma']  = 5./3.
+        self.uni['tg']     = 5.77e9 # K
+        self.uni['l']      = 1.0e8 # Mm -> cm
+        self.uni['rho']    = 1.67e-9 # gr cm^-3 
+        self.uni['u']      = 6.9e8 # cm/s
+        self.uni['b']      = 100.0 # Gauss
+        self.uni['t']      = 0.145 # seconds
+
+        # Units and constants in SI
+
+        convertcsgsi(self)
+
+        globalvars(self)
+
+        self.uni['n']      = self.uni['rho'] / self.m_p / 2. # cm^-3

helita/sim/matsumotosav.py
@@ -32,8 +32,9 @@ class Matsumotosav:
     self.savefile = rsav(os.path.join(fdir,rootname+'{:06d}'.format(snap)+'.sav'))
     self.snap     = snap
     self.sel_units= sel_units
-    self.verbose = verbose
-
+    self.verbose  = verbose
+    self.uni      = Matsumotosav_units()
+    
     self.time     = self.savefile['v']['time'][0]
     self.grav     = self.savefile['v']['gx'][0]
     self.gamma    = self.savefile['v']['gm'][0]
@@ -55,7 +56,6 @@ class Matsumotosav:
     self.hion = False # This will not allow to use HION from Bifrost in load
 
     self.genvar()
-    self.units()
 
 
   def get_var(self,var,snap=None, iix=None, iiy=None, iiz=None, layout=None): 
@@ -104,8 +104,8 @@ class Matsumotosav:
         varu=var.replace('x','')
         varu=varu.replace('y','')
         varu=varu.replace('z','')
-        if (var in self.varn.keys()) and (varu in self.uni.keys()): 
-          cgsunits = self.uni[varu]
+        if (var in self.varn.keys()) and (varu in self.uni.uni.keys()): 
+          cgsunits = self.uni.uni[varu]
         else: 
           cgsunits = 1.0
       else: 
@@ -130,11 +130,14 @@ class Matsumotosav:
       self.data = load_noeos_quantities(self,var)
 
       if np.shape(self.data) == ():
-        self.data = load_quantities(self,var,PLASMA_QUANT='',
-                CYCL_RES='', COLFRE_QUANT='', COLFRI_QUANT='',
-                IONP_QUANT='', EOSTAB_QUANT='', TAU_QUANT='',
-                DEBYE_LN_QUANT='', CROSTAB_QUANT='',
-                COULOMB_COL_QUANT='', AMB_QUANT='')
+        self.data = load_quantities(self,var,PLASMA_QUANT='', CYCL_RES='',
+                COLFRE_QUANT='', COLFRI_QUANT='', IONP_QUANT='',
+                EOSTAB_QUANT='', TAU_QUANT='', DEBYE_LN_QUANT='',
+                CROSTAB_QUANT='', COULOMB_COL_QUANT='', AMB_QUANT='', 
+                HALL_QUANT='', BATTERY_QUANT='', SPITZER_QUANT='', 
+                KAPPA_QUANT='', GYROF_QUANT='', WAVE_QUANT='', 
+                FLUX_QUANT='', CURRENT_QUANT='', COLCOU_QUANT='',  
+                COLCOUMS_QUANT='', COLFREMX_QUANT='')
 
         # Loading arithmetic quantities
         if np.shape(self.data) == ():
@@ -155,27 +158,6 @@ class Matsumotosav:
    
     return self.data
   
-  def units(self): 
-    '''
-    Units and constants in cgs
-    '''
-    self.uni={}
-    self.uni['tg']     = 1.0 # K
-    self.uni['l']      = 1.0e8 # Mm -> cm
-    self.uni['rho']    = 1.0 # gr cm^-3 
-    self.uni['n']      = 1.0 # cm^-3
-    self.uni['u']      = 1.0 # cm/s
-    self.uni['b']      = 1.0 # Gauss
-    self.uni['t']      = 1.0 # seconds
-
-    # Units and constants in SI
-    convertcsgsi(self)
-
-    globalvars(self)
-
-
-    self.uni['gamma']  = self.gamma 
-
  
   def genvar(self): 
     '''
@@ -229,4 +211,30 @@ class Matsumotosav:
     # also velocities. 
 
     return var
+
+class Cipmocct_units(object): 
+
+    def __init__(self):
+
+
+        '''
+        Units and constants in cgs
+        '''
+        self.uni={}
+        self.uni['tg']     = 1.0 # K
+        self.uni['l']      = 1.0e8 # Mm -> cm
+        self.uni['rho']    = 1.0 # gr cm^-3 
+        self.uni['n']      = 1.0 # cm^-3
+        self.uni['u']      = 1.0 # cm/s
+        self.uni['b']      = 1.0 # Gauss
+        self.uni['t']      = 1.0 # seconds
+
+        # Units and constants in SI
+        convertcsgsi(self)
+
+        globalvars(self)
+
+
+        self.uni['gamma']  = self.gamma 
+
 

helita/sim/pypluto.py
@@ -774,7 +774,7 @@ class PlutoData(object):
     self.snap = snap 
     self.sel_units =sel_units 
     self.verbose = verbose
-
+    self.uni = Pypluto_units()
     self.info = pload(snap,w_dir=fdir,datatype=datatype)
     self.x = self.info.x1
     self.y = self.info.x2
@@ -799,7 +799,6 @@ class PlutoData(object):
     self.hion = False # This will not allow to use HION from Bifrost in load
     
     #self.time =  params['time'] # No uniforme (array)
-    self.units()
     self.genvar()
 
   def get_var(self,var,*args, snap=None, iix=None, iiy=None, iiz=None, layout=None, **kwargs): 
@@ -835,20 +834,18 @@ class PlutoData(object):
       self.snap = snap
     
     if var in self.varn.keys(): 
-
       if self.sel_units == 'cgs': 
         varu=var.replace('x','')
         varu=varu.replace('y','')
         varu=varu.replace('z','')
-        if (var in self.varn.keys()) and (varu in self.uni.keys()): 
-          cgsunits = self.uni[varu]
+        if (var in self.varn.keys()) and (varu in self.uni.uni.keys()): 
+          cgsunits = self.uni.uni[varu]
         else: 
           cgsunits = 1.0
       else: 
         cgsunits = 1.0
-
-        self.data = getattr(self.info,self.varn[var]) * cgsunits
-
+    
+      self.data = getattr(self.info,self.varn[var]) * cgsunits
     else: 
 
       self.get_comp_vars(var, *args, snap=snap, iix=iix, iiy=iiy, iiz=iiz, layout=layout, **kwargs)
@@ -860,18 +857,20 @@ class PlutoData(object):
         self.data = load_noeos_quantities(self,var)
 
         if np.shape(self.data) == ():
-          self.data = load_quantities(self,var,PLASMA_QUANT='',
-                  CYCL_RES='', COLFRE_QUANT='', COLFRI_QUANT='',
-                  IONP_QUANT='', EOSTAB_QUANT='', TAU_QUANT='',
-                  DEBYE_LN_QUANT='', CROSTAB_QUANT='',
-                  COULOMB_COL_QUANT='', AMB_QUANT='')
+          self.data = load_quantities(self,var,PLASMA_QUANT='', CYCL_RES='',
+                COLFRE_QUANT='', COLFRI_QUANT='', IONP_QUANT='',
+                EOSTAB_QUANT='', TAU_QUANT='', DEBYE_LN_QUANT='',
+                CROSTAB_QUANT='', COULOMB_COL_QUANT='', AMB_QUANT='', 
+                HALL_QUANT='', BATTERY_QUANT='', SPITZER_QUANT='', 
+                KAPPA_QUANT='', GYROF_QUANT='', WAVE_QUANT='', 
+                FLUX_QUANT='', CURRENT_QUANT='', COLCOU_QUANT='',  
+                COLCOUMS_QUANT='', COLFREMX_QUANT='')
 
           # Loading arithmetic quantities
           if np.shape(self.data) == ():
             if self.verbose: 
               print('Loading arithmetic variable',end="\r",flush=True)
             self.data = load_arithmetic_quantities(self,var) 
-  
     if var == '': 
 
       print(help(self.get_var))
@@ -881,7 +880,6 @@ class PlutoData(object):
       print(self.description['ALL']) 
 
       return None
-   
     return self.data
 
 
@@ -892,41 +890,13 @@ class PlutoData(object):
     if var == 'tg': 
       
       if self.sel_units == 'cgs': 
-          cgsunits = self.uni['tg']
+          cgsunits = self.uni.uni['tg']
       else: 
           cgsunits = 1.0
-      print(cgsunits)
-      rho=self.get_var('rho',snap=snap)
-      pres =  self.get_var('pg',snap=snap) 
-      self.data = rho * pres * cgsunits
+
+      self.data = self.get_var('rho',snap=snap) * self.get_var('pg',snap=snap)  * cgsunits
     else: 
       self.data = None
-  
-  def units(self): 
-    '''
-    Units and constants in cgs
-    '''
-    self.uni={}
-    self.uni['tg']     = 1.0e6 # K
-    self.uni['l']      = 1.0e8 # cm 
-    self.uni['rho']    = 1.0e-15 # gr cm^-3 
-    self.uni['kboltz'] =  1.380658E-16 # Boltzman's cst. [erg/K]
-    self.uni['proton'] =  const.m_n / const.gram        # 1.674927471e-24
-    self.uni['R_spec'] = self.uni['kboltz'] / (0.5e0 * self.uni['proton'])
-    self.uni['u']      = np.sqrt(self.uni['R_spec']*self.uni['tg']) # cm/s
-    self.uni['pg']     = self.uni['rho'] * self.uni['u']**2
-    self.uni['b']      = np.sqrt(4.0 * np.pi * self.uni['pg']) # Gauss
-    self.uni['t']      = self.uni['l']/self.uni['u'] # seconds
-
-    # Units and constants in SI
-    convertcsgsi(self)
-
-    globalvars(self)
-
-
-    self.uni['j']      = self.uni['b']/self.uni['l']*self.clight # current density
-    self.uni['gr']     = 2.7e4 # solar gravity in cgs
-    self.uni['gc']     = self.uni['gr'] * self.uni['l'] / self.uni['u'] ** 2 # solar gravity in Code units. 
       
  
   def genvar(self): 
@@ -977,6 +947,37 @@ class PlutoData(object):
 
     return get_var(varname,snap=snap)
 
+
+class Pypluto_units(object): 
+
+    def __init__(self,filename='mhd.in',fdir='./',verbose=True):
+    
+        '''
+        Units and constants in cgs
+        '''
+
+        self.uni={}
+
+        self.uni['tg']     = 1.0e6 # K
+        self.uni['l']      = 1.0e8 # cm 
+        self.uni['rho']    = 1.0e-15 # gr cm^-3 
+        self.uni['kboltz'] =  1.380658E-16 # Boltzman's cst. [erg/K]
+        self.uni['proton'] =  const.m_n / const.gram        # 1.674927471e-24
+        self.uni['R_spec'] = self.uni['kboltz'] / (0.5e0 * self.uni['proton'])
+        self.uni['u']      = np.sqrt(self.uni['R_spec']*self.uni['tg']) # cm/s
+        self.uni['pg']     = self.uni['rho'] * self.uni['u']**2
+        self.uni['b']      = np.sqrt(4.0 * np.pi * self.uni['pg']) # Gauss
+        self.uni['t']      = self.uni['l']/self.uni['u'] # seconds
+
+        # Units and constants in SI
+        convertcsgsi(self)
+
+        globalvars(self)
+
+
+        self.uni['j']      = self.uni['b']/self.uni['l']*self.clight # current density
+        self.uni['gr']     = 2.7e4 # solar gravity in cgs
+        self.uni['gc']     = self.uni['gr'] * self.uni['l'] / self.uni['u'] ** 2 # solar gravity in Code units. 
 
 class Tools(object):
   """

helita/sim/radyn.py
@@ -33,6 +33,8 @@ class radyn:
     self.sel_units= sel_units
     self.verbose = verbose
     
+    self.uni = Radyn_units()
+    
     self.dx = 1.0
     self.dy = 1.0
     self.dz = np.copy(self.z)
@@ -52,7 +54,6 @@ class radyn:
     self.cstagop = False # This will not allow to use cstagger from Bifrost in load
     self.hion = False # This will not allow to use HION from Bifrost in load
 
-    self.units()
     self.genvar()
 
   def get_var(self, var, iix=None, iiy=None, iiz=None, layout=None): 
@@ -86,8 +87,8 @@ class radyn:
         varu=var.replace('x','')
         varu=varu.replace('y','')
         varu=varu.replace('z','')
-        if (var in self.varn.keys()) and (varu in self.uni.keys()): 
-          cgsunits = self.uni[varu]
+        if (var in self.varn.keys()) and (varu in self.uni.uni.keys()): 
+          cgsunits = self.uni.uni[varu]
         else: 
           cgsunits = 1.0
       else: 
@@ -96,11 +97,14 @@ class radyn:
       self.data = self.rdobj.__getattr__(varname) * cgsunits
     except: 
 
-      self.data = load_quantities(self,var,PLASMA_QUANT='',
-                  CYCL_RES='', COLFRE_QUANT='', COLFRI_QUANT='',
-                  IONP_QUANT='', EOSTAB_QUANT='', TAU_QUANT='',
-                  DEBYE_LN_QUANT='', CROSTAB_QUANT='',
-                  COULOMB_COL_QUANT='', AMB_QUANT='')
+      self.data = load_quantities(self,var,PLASMA_QUANT='', CYCL_RES='',
+                COLFRE_QUANT='', COLFRI_QUANT='', IONP_QUANT='',
+                EOSTAB_QUANT='', TAU_QUANT='', DEBYE_LN_QUANT='',
+                CROSTAB_QUANT='', COULOMB_COL_QUANT='', AMB_QUANT='', 
+                HALL_QUANT='', BATTERY_QUANT='', SPITZER_QUANT='', 
+                KAPPA_QUANT='', GYROF_QUANT='', WAVE_QUANT='', 
+                FLUX_QUANT='', CURRENT_QUANT='', COLCOU_QUANT='',  
+                COLCOUMS_QUANT='', COLFREMX_QUANT='')
 
       if np.shape(self.data) == ():
         if self.verbose: 
@@ -119,27 +123,6 @@ class radyn:
    
     return self.data
 
-
-  def units(self): 
-    '''
-    Units and constants in cgs
-    '''
-    self.uni={}
-
-    self.uni['tg']     = 1.0
-    self.uni['l']      = 1.0
-    self.uni['n']      = 1.0
-    self.uni['rho']    = 1.0
-    self.uni['u']      = 1.0
-    self.uni['b']      = 1.0
-    self.uni['t']      = 1.0 # seconds
-    self.uni['j']      = 1.0
-
-    # Units and constants in SI
-    convertcsgsi(self)
-
-    globalvars(self)
- 
 
   def genvar(self): 
     '''
@@ -194,3 +177,24 @@ class radyn:
 
     return var
 
+class Laresav_units(object): 
+
+    def __init__(self):
+        '''
+        Units and constants in cgs
+        '''
+        self.uni={}
+
+        self.uni['tg']     = 1.0
+        self.uni['l']      = 1.0
+        self.uni['n']      = 1.0
+        self.uni['rho']    = 1.0
+        self.uni['u']      = 1.0
+        self.uni['b']      = 1.0
+        self.uni['t']      = 1.0 # seconds
+        self.uni['j']      = 1.0
+
+        # Units and constants in SI
+        convertcsgsi(self)
+
+        globalvars(self)