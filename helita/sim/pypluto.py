dabe4748f331761245595763271ac07ceb2bbd4e
juan <jumasy1980@gmail.com>
2021-01-04 11:33:16 +0000

    paolounits

--- helita/sim/pypluto.py
+++ helita/sim/pypluto.py
@@ -765,7 +765,7 @@ class PlutoData(object):
       snapshot number 
   """
   def __init__(self, rootname, snap, fdir='./', datatype='dbl', 
-              verbose=True, sel_units = 'cgs', *args, **kwargs):
+              verbose=True, sel_units = 'cgs', typemodel='Kostas', *args, **kwargs):
 
     #super(PlutoData, self).__init__(it, w_dir=fdir, datatype=datatype, *args, **kwargs)
 
@@ -774,7 +774,11 @@ class PlutoData(object):
     self.snap = snap 
     self.sel_units =sel_units 
     self.verbose = verbose
-    self.uni = Pypluto_units()
+    self.typemodel = typemodel
+    if self.typemodel == 'Kostas': 
+        self.uni = Pypluto_units()
+    elif (self.typemodel == 'Paolo'): 
+        self.uni = Pypluto_paolo_units()
     self.info = pload(snap,w_dir=fdir,datatype=datatype)
     self.x = self.info.x1
     self.y = self.info.x2
@@ -865,7 +869,7 @@ class PlutoData(object):
         # Loading quantities
         if self.verbose: 
           print('Loading composite variable',end="\r",flush=True)
-        self.data = load_noeos_quantities(self,var, **kargs)
+        self.data = load_noeos_quantities(self,var, **kwargs)
 
         if np.shape(self.data) == ():
           self.data = load_quantities(self,var,PLASMA_QUANT='', CYCL_RES='',
@@ -875,13 +879,13 @@ class PlutoData(object):
                 HALL_QUANT='', BATTERY_QUANT='', SPITZER_QUANT='', 
                 KAPPA_QUANT='', GYROF_QUANT='', WAVE_QUANT='', 
                 FLUX_QUANT='', CURRENT_QUANT='', COLCOU_QUANT='',  
-                COLCOUMS_QUANT='', COLFREMX_QUANT='', **kargs)
+                COLCOUMS_QUANT='', COLFREMX_QUANT='', **kwargs)
 
           # Loading arithmetic quantities
           if np.shape(self.data) == ():
             if self.verbose: 
               print('Loading arithmetic variable',end="\r",flush=True)
-            self.data = load_arithmetic_quantities(self, var, **kargs) 
+            self.data = load_arithmetic_quantities(self, var, **kwargs) 
     if var == '': 
 
       print(help(self.get_var))
@@ -1004,6 +1008,40 @@ class Pypluto_units(object):
 
         globalvars(self)
 
+
+        self.uni['j']      = self.uni['b']/self.uni['l']*self.clight # current density
+        self.uni['gr']     = 2.7e4 # solar gravity in cgs
+        self.uni['gc']     = self.uni['gr'] * self.uni['l'] / self.uni['u'] ** 2 # solar gravity in Code units. 
+
+
+class Pypluto_paolo_units(object): 
+
+    def __init__(self):
+    
+        '''
+        Units and constants in cgs
+        '''
+
+        rsun= 6.960e10
+        proton_mass=1.672623110e-24
+        self.uni={}
+
+        self.uni['l']      = 6.960e10 # cm 
+        self.uni['u']      = 1.0e7 # cm/s
+        self.uni['rho']    = 2e10*1.2650*proton_mass # gr cm^-3 
+        self.uni['b']      = self.uni['u']*np.sqrt(4.0 * np.pi * self.uni['rho']) # Gauss
+        self.uni['t']      = self.uni['l']/self.uni['u'] # seconds
+        self.uni['pg']     = self.uni['rho'] * self.uni['u']**2 # erg cm^-3 
+        #self.uni['tg']     = 1.203e6/2.0*1.26506 # K
+        self.uni['tg']     =  1.3747056e22 # K        
+        self.uni['kboltz'] =  1.380658E-16 # Boltzman's cst. [erg/K]
+        self.uni['proton'] =  const.m_n / const.gram        # 1.674927471e-24
+        self.uni['R_spec'] = self.uni['kboltz'] / (0.5e0 * self.uni['proton'])
+
+        # Units and constants in SI
+        convertcsgsi(self)
+
+        globalvars(self)
 
         self.uni['j']      = self.uni['b']/self.uni['l']*self.clight # current density
         self.uni['gr']     = 2.7e4 # solar gravity in cgs
