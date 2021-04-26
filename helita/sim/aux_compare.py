1276812f0db36c095ffd339e6fc50cb698696d45
Samuel Evans <sevans7@bu.edu>
2021-04-26 11:07:42 -0400

    (aux_compare.py) [ADD] compare_all allows to easily compare multiple vars aux values and helita values. [DOC] add compare_all example.

--- helita/sim/aux_compare.py
+++ helita/sim/aux_compare.py
@@ -3,7 +3,39 @@ Created by Sam Evans on Apr 24 2021
 
 purpose: easily compare values between helita and aux vars from a simulation.
 
-Example usage:
+Highest-level use-case: compare all the aux vars with their helita counterparts!
+    #<< input:
+    from helita.sim import aux_compare as axc
+    from helita.sim import ebysus as eb
+    dd = eb.EbysusData(...)        # you must fill in the ... as appropriate.
+    c = axc.compare_all(dd)
+
+    #>> output:
+             >->->->->->->-> initiate comparison for auxvar = etg <-<-<-<-<-<-<-<
+
+    auxvar etg            min= 4.000e+03, mean= 4.000e+03,  max= 4.000e+03
+    helvar  tg   -1       min= 4.000e+03, mean= 4.000e+03,  max= 4.000e+03;   mean ratio (aux / helita):  1.000e+00
+    ----------------------------------------------------------------------------------------------------------------------
+
+    comparison_result(N_differ=0, N_total=1, runtime=0.0020618438720703125) 
+
+
+         >->->->->->->-> initiate comparison for auxvar = mm_cnu <-<-<-<-<-<-<-<
+
+    auxvar mm_cnu  ( 1, 1) ( 1, 2)   min= 8.280e+05, mean= 8.280e+05,  max= 8.280e+05
+    helvar  nu_ij  ( 1, 1) ( 1, 2)   min= 8.280e+05, mean= 8.280e+05,  max= 8.280e+05;   mean ratio (aux / helita):  1.000e+00
+    ---------------------------------------------------------------------------------------------------------------------------------
+
+    ... << (more lines of output, which we are not showing you in this file, to save space.)
+
+    #<< more input:
+    print(c)
+
+    #>> more output:
+    {'N_compare': 30, 'N_var': 8, 'N_differ': 4, 'N_diffvar': 1, 'N_error': 1,
+    'errors': [FileNotFoundError(2, 'No such file or directory')], 'runtime': 1.581925868988037}
+
+High-level use-case: compare a single aux var with its helita counterpart!
     #<< input:
     from helita.sim import aux_compare as axc
     from helita.sim import ebysus as eb
@@ -35,6 +67,10 @@ Example usage:
 # when ispecies < 0 or jspecie < 0 (i.e. for electrons), they may be shown as "specie" instead of "(ispecie, ilevel)".
 """
 
+# import built-in
+from collections import namedtuple
+import time
+
 # import internal modules
 from . import fluid_tools
 
@@ -45,20 +81,33 @@ except ImportError:
     fl = None
     warnings.warn('failed to import at_tools.fluids; some functions in helita.sim.aux_compare may crash')
 
+
+''' ----------------------------- lookup helita counterpart to aux var ----------------------------- '''
+
+# dict of defaults for converting from auxvar to helita var (aka "helvar").
 AUXVARS = {
     # aux var   : helita var. if tuple, v[1] is required ifluid or mf_ispecies.
                                      #  v[2] (if it exists) jfluid or mf_jspecies.
-    'etg'       : ('tg', -1),
-    'mfe_tg'    : 'tg',
-    'ex'        : 'efx',
-    'ey'        : 'efy',
-    'ez'        : 'efz',
-    'mm_cnu'    : 'nu_ij',
-    'mfr_nu_es' : ('nu_ij', -1),
-} 
+    'etg'       : ('tg', -1),    # electron temperature
+    'mfe_tg'    : 'tg',          #  fluid   temperature
+    'mfr_nu_es' : ('nu_ij', -1), # electron-fluid collision frequency
+    'mm_cnu'    : 'nu_ij',       #  fluid - fluid collision frequency
+    
+}
+# add each of these plus an axis to AUXVARS.
+# e.g. {'e': 'ef'} --> {'ex': 'efx', 'ey': 'efy', 'ez': 'efz'}.
+AUX_AXIAL_VARS = {
+    'e'         : 'ef',
+    'eu'        : 'ue',
+}
+AXES = ['x', 'y', 'z']
+# add the axial vars to auxvars.
+for (aux, hel) in AUX_AXIAL_VARS.items():
+    AUXVARS.update({aux+x: hel+x for x in AXES})
 
 def get_helita_var(auxvar):
     return AUXVARS[auxvar]
+
 
 ''' ----------------------------- get_var for helita & aux ----------------------------- '''
 
@@ -236,7 +285,7 @@ def iter_get_var(obj, auxvar, helvar=None, fluids=None, f=lambda fluid: fluid,
         fluids, f = _get_fluids_and_f(obj, fluids, f)
     iterfluids = _iter_fluids(fluids, loopfluids, ordered=ordered, allow_same=allow_same)
     for auxfluids_dict, helfluids_dict in iterfluids:
-        auxcall, helcall = _setup_fluid_kw(auxvar, callsig, auxfluids_dict, helfluids_dict, f=f, **kw__get_var)
+        auxcall, helcall = _setup_fluid_kw(auxvar, callsig, auxfluids_dict, helfluids_dict, f=f)
         auxfluidsSL = auxcall[1].copy()
         helfluidsSL = helcall[1].copy()
         auxcall[1].update(**kw__get_var)
@@ -365,17 +414,138 @@ def prettyprint_comparison(x, printout=True, prefix=True, underline=True,
 
 ''' ----------------------------- high-level comparison interface ----------------------------- '''
 
+comparison_result = namedtuple('comparison_result', ('N_differ', 'N_total', 'runtime')) 
+
 @fluid_tools.maintain_fluids # restore dd.ifluid and dd.jfluid after finishing compare().
-def compare(obj, auxvar, **kwargs):
+def compare(obj, auxvar, helvar=None, fluids=None, **kwargs):
     '''compare values of auxvar with appropriate helita var, for obj.
     **kwargs propagate to iter_get_var, obj.get_var, and prettyprint_comparison.
 
-    return number of times helita and auxvar gave different mean results (by more than rattol).
-    0 is good, it means helita & auxvar agreed on everything! :)
+    involves looping through fluids:
+        none (nfluid=0, e.g. 'ex'),
+        one  (nfluid=1, e.g. 'tg'), or
+        two  (nfluid=2, e.g. 'nu_ij').
+
+    Parameters
+    ----------
+    helvar: None (default)or str or tuple
+        helita var corresponding to auxvar. E.g. 'efx' for auxvar='ex'.
+        it is assumed that helvar and auxvar use the same number of fluids.
+        For example, 'mfe_tg' and 'tg' each use one fluid.
+        For some vars, there is a slight hiccup. Example: 'etg' and 'tg'.
+            'etg' is equivalent to 'tg' only when mf_ispecies=-1.
+        To accomodate such cases, we allow a tuple such as ('tg', -1) for helvar.
+        type of helvar, and explanations below:
+            None -> use default. (but not all auxvars have an existing default.)
+                    all defaults which exist are hard-coded in helita.sim.aux_compare.
+                    when default fails, use non-None helvar,
+                    or edit helita.sim.aux_compare.AUXVARS to add default.
+            str  -> use var = helvar. This one should do exactly what you expect.
+                    Note that for this case, auxvar and helvar must depend on the
+                    exact same fluids (e.g. both depend on just ifluid).
+            tuple-> use var = helvar[0]. The remaining items in the tuple
+                    will force fluid kwargs for helvar. ints for mf_species;
+                    tuples force fluid. Example: ('nu_ij', -1) forces mf_ispecies=-1,
+                    and because nu_ij depends on 2 fluids (according to obj.vardict),
+                    we still need to enter one fluid. So we will loop through
+                    fluids, passing each fluid to helvar as jfluid, and auxvar as ifluid.
+    
+    fluids: None (default) or iterable (e.g. list)
+        None     -> get fluids using obj. fluids = at_tools.fluids.Fluids(dd=obj)
+        iterable -> use these fluids. Should be tuples of (specie, level),
+                    example: fluids = [(1,2),(2,2),(2,3)]
+                    See aux_compare.iter_get_var for more documentation.
+
+    Returns
+    -------
+    returns namedtuple (N_differ, N_total, runtime), where:
+        N_differ = number of times helita and auxvar gave
+                   different mean results (differing by more than rattol).
+                   0 is good, it means helita & auxvar agreed on everything! :)
+        N_total  = total number of values compared. example:
+                   if we compared 'mfe_tg' and 'tg' for ifluid in 
+                   [(1,1),(1,2),(2,3)], we will have N_total==3.
+        runtime  = time it took to run, in seconds.
     '''
+    now = time.time()
     N_warnings = 0
-    for x in iter_get_var(obj, auxvar, **kwargs):
+    N_total    = 0
+    for x in iter_get_var(obj, auxvar, helvar=helvar, fluids=fluids, **kwargs):
+        N_total += 1
         _, warned = prettyprint_comparison(x, return_warned=True, **kwargs)
         if warned:
             N_warnings += 1
-    return N_warnings
+    return comparison_result(N_warnings, N_total, time.time() - now)
+
+def _get_aux_vars(obj):
+    '''returns list of vars in aux based on obj.'''
+    return obj.params['aux'][obj.snapInd].split()
+
+def compare_all(obj, aux=None, verbose=2, **kwargs):
+    '''compare all aux vars with their corresponding values in helita.
+
+    each comparison involves looping through fluids:
+        none (nfluid=0, e.g. 'ex'),
+        one  (nfluid=1, e.g. 'tg'), or
+        two  (nfluid=2, e.g. 'nu_ij').
+    
+    Parameters
+    ----------
+    obj: an EbysusData object.
+        (or any object with get_var method.)
+    aux: None (default) or a list of strs
+        the list of aux vars to compare.
+        None -> get the list from obj (via obj.params['aux']).
+    verbose: 2 (default), 1, or 0
+        2 -> full print info
+        1 -> print some details but set printout=False (unless printout is in kwargs)
+        0 -> don't print anything.
+    **kwargs:
+        extra kwargs are passed to compare(). (i.e.: helita.sim.aux_compare.compare)
+
+    Returns
+    -------
+    returns dict with contents:
+        N_compare = number of actual values we compared.
+                    (one for each set of fluids used for each (auxvar, helvar) pair)
+        N_var     = number of vars we tried to run comparisons for.
+                    (one for each (auxvar, helvar) pair)
+        N_differ  = number of times helita and auxvar gave
+                    different mean results (by more than rattol)
+        N_diffvar = number of vars for which helita and auxvar gave
+                    different mean results at least once.
+        N_error   = number of times compare() crashed due to error.
+        errors    = list of errors raised.
+        runtime   = time it took to run, in seconds.
+
+    A 100% passing test looks like N_differ == N_error == 0.
+    '''
+    now = time.time()
+    printout = kwargs.pop('printout', (verbose >= 2) ) # default = (verbose>=2)
+    x = dict(N_compare=0, N_var=0, N_differ=0, N_diffvar=0, N_error=0, errors=[])
+    auxvars = _get_aux_vars(obj) if aux is None else aux
+    for aux in auxvars:
+        if verbose:
+            banner = '     >->->->->->->-> {} <-<-<-<-<-<-<-<'
+            if printout: banner += '\n'
+            print(banner.format('initiate comparison for auxvar = {}'.format(aux)))
+        x['N_var'] +=1
+        try:
+            comp = compare(obj, aux, printout=printout, **kwargs)
+        except Exception as exc:
+            x['N_error'] += 1
+            x['errors']  += [exc]
+            if verbose >= 1:
+                print('>>>', repr(exc))
+        else:
+            x['N_compare'] += comp[1]
+            x['N_differ']  += comp[0]
+            x['N_diffvar'] += (comp[0] > 0)
+        if printout: print() # print a single new line
+        if verbose >= 1:
+            print(comp, '\n')
+        if printout: print() # print a single new line
+    x['runtime'] = time.time() - now
+    return x
+
+
